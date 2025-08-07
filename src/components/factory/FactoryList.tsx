5f8a004c25be567846a8efa24dd2c44e64a4756b
Paulo Leite <31547227+pauloleitte@users.noreply.github.com>
2025-08-07 14:17:53 -0300

    feat: add create at

--- src/components/factory/FactoryList.tsx
+++ src/components/factory/FactoryList.tsx
@@ -1,10 +1,11 @@
-import { List, Datagrid, TextField, ReferenceField } from "react-admin";
+import { List, Datagrid, TextField, ReferenceField, DateField } from "react-admin";
 import React from "react";
 
 export const FactoryList: React.FC = (props) => (
   <List {...props}>
     <Datagrid rowClick="show">
       <TextField source="dimonaOrderId" />
+      <DateField source="createdAt"  />
       <ReferenceField source="orderId" reference="orders">
         <TextField source="id" />
       </ReferenceField>
